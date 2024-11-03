3d0f281af7380f78edb5871faac773c3db92d9ae
roshnirabindrasahoo <sahooroshni1210@gmail.com>
2024-11-02 17:09:08 -0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -22,3 +22,25 @@ class Value:
         out._backward = _backward
 
         return out
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+    def _backward():
+        self.grad += other.data * out.grad
+        other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+    def _backward():
+        self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
+
+        return out
+    
