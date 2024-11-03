37a78e7a22aba3e3af3695dad3340013e2b16c62
roshnirabindrasahoo <sahooroshni1210@gmail.com>
2024-11-02 17:04:24 -0700

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,3 +1,11 @@
 import random
 from engine import Value
+class Module:
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
 
