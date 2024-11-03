45a250113566a16a04c87420eeaa6aee79aae431
roshnirabindrasahoo <sahooroshni1210@gmail.com>
2024-11-02 17:10:05 -0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -43,4 +43,26 @@ class Value:
         out._backward = _backward
 
         return out
+        
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+    def _backward():
+        self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def backward(self):
+
+    # topological order all of the children in the graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
     
